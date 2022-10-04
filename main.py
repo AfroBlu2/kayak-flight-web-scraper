d350e5ddfcdd522369ae079d2b4e50bd6217faa7
Kenton Mail <kentonanders@gmail.com>
2022-10-04 11:21:46 -0400

    started start_kayak function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from time import sleep, strftime
 from random import randint
 import pandas as pd
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import smtplib
 from email.mime.multipart import MIMEMultipart
@@ -11,12 +12,55 @@ chromedriver_path = '/Users/kenton/Documents/Programming/Personal Projects/Web S
 driver = webdriver.Chrome(executable_path=chromedriver_path)
 sleep(2)
 
-kayak = 'https://www.ca.kayak.com/flights/YQB-ATH/2023-03-12-flexible-3days/2023-03-18-flexible-3days?sort=bestflight_a'
-driver.get(kayak)
+
 
 # MIGHT NEED TO TARGET THIS IN ANOTHER WAY IF CLASS ENDS UP BEING DYNAMIC GOOGLE SEARCH XPATHS
-xp_popup_close = 'bBPb-closeIcon'
-print(xp_popup_close)
-driver.find_element_by_class_name(xp_popup_close).click()
+
+
+def close_popup():
+    try:
+        xp_popup_close = '.bBPb-closeIcon'
+        driver.find_element_by_css_selector(xp_popup_close).click()
+    except Exception as e:
+        print('No popup displayed')
+        return
+
+def page_scrape():
+    """
+    Here we scrape
+
+    """
+
+
+def start_kayak(city_from, city_to, date_start, date_end):
+    """
+    This is the main function that starts the bot
+
+        city_from, city_to : string (airport IAIA codes, three letters)
+        date_start, date_end : string (date format YYYY-MM-DD)
+    """
+    kayak = ('https://www.ca.kayak.com/flights/' + city_from + '-' + city_to + '/' + date_start + '-flexible-3days/' + date_end + '-flexible-3days?sort=bestflight_a')
+    driver.get(kayak)
+    sleep(randint(8,10))
+
+    close_popup()
+    sleep(randint(60,95))
+
+    print('Starting first scrape...')
+
+
+
+
+start_kayak('YQB', 'ATH', '2023-03-12', '2023-03-18')
+
+
+
+
+
+
+# FIND CHEAPEST FLIGHT
+sleep(2)
+cheapest_flight = ''
+
 
 
